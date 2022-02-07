1079a29de07c743a98d76fcbc80198f44c939d62
Ketsman Aleksandr <ketsman94@mail.ru>
2022-02-07 21:31:07 +0300

    v2

--- main.cpp
+++ main.cpp
@@ -1,41 +1,62 @@
 #include <iostream>
+#include <utility>
+
 class Animal
 {
 public:
-    virtual void voice(Animal* meetAnimal) = 0;
+    std::string name = "unknown";
+    virtual void WelcomeVoice() = 0;
+    virtual void ThreatVoice() = 0;
+    virtual void ReactTo(Animal* animal) = 0;
+    virtual std::string getName() = 0;
 };
 
 class Cat: public Animal
 {
 public:
-    void voice(Animal* meetAnimal)
-    {
-        if (this == meetAnimal) std::cout << "Purr";
-        else std::cout<< "Meow";
+    Cat() {
+        name = "Cat";
+    }
+    void ReactTo(Animal* animal) override{
+        if (this->getName() == animal->getName()) WelcomeVoice();
+        else ThreatVoice();
+    };
+    void WelcomeVoice() override { std::cout << "Purr";}
+    void ThreatVoice() override { std::cout << "Meow";}
+    std::string getName() override{
+        return name;
     }
 };
+
 
 class Dog: public Animal
 {
 public:
-    void voice(Animal* meetAnimal)
-    {
-        if (this == meetAnimal) std::cout << "Woof";
-        else std::cout<< "Bark";
+    Dog() {
+        name = "Dog";
+    }
+    void ReactTo(Animal* animal) override{
+        if (this->getName() == animal->getName()) WelcomeVoice();
+        else ThreatVoice();
+    };
+    void WelcomeVoice() override { std::cout << "Woof";}
+    void ThreatVoice() override { std::cout << "Bark";}
+    std::string getName() override{
+        return name;
     }
 };
 
+
 void Meeting(Animal* a, Animal* b)
 {
-    a->voice(b);
+    a->ReactTo(b);
     std::cout << " - ";
-    b->voice(a);
+    b->ReactTo(a);
 }
 
-
 int main() {
-    Animal* b = new Cat();
     Animal* a = new Cat();
+    Animal* b = new Dog();
     Meeting(a, b);
 
     return 0;
